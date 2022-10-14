31b50b6fdbde929fdd306de1e6d5e92347cbdb48
Laurent Schaeffer 👨‍💻 <laurent.schaeffer313@gmail.com>
2022-10-15 10:22:47 +1100

    fix(DomaineDetailsPage): :art: Added a Widget to display each specific info of a domain
    #21

--- lib/page/domaine_detail_page.dart
+++ lib/page/domaine_detail_page.dart
@@ -2,6 +2,7 @@ import 'package:add_2_calendar/add_2_calendar.dart';
 import 'package:domaine_nc_mobile/model/domaine_info.dart';
 import 'package:domaine_nc_mobile/model/domaine_search_result.dart';
 import 'package:domaine_nc_mobile/service/domaine_service.dart';
+import 'package:domaine_nc_mobile/widget/custom_list_tile.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:skeletons/skeletons.dart';
@@ -12,10 +13,6 @@ class DomaineDetailPage extends StatefulWidget {
     required this.domaineSearchResult,
   });
   final DomaineSearchResult domaineSearchResult;
-  final styleTextTitle = const TextStyle(
-    fontWeight: FontWeight.bold,
-    fontSize: 20,
-  );
   final styleDefaultText = const TextStyle(
     color: Colors.black45,
   );
@@ -127,53 +124,62 @@ class _DomaineDetailPage extends State<DomaineDetailPage> {
     ];
   }
 
-  ListTile _constructWidgetInfo(Icon icon, String title, String? subtitle,
-      {bool isTitle = false}) {
-    return ListTile(
-      contentPadding:
-          isTitle ? const EdgeInsets.symmetric(horizontal: 0) : null,
-      leading: icon,
-      title: Text(
-        title,
-        style: TextStyle(
-          fontWeight: FontWeight.bold,
-          fontSize: isTitle ? 35 : null,
-        ),
-      ),
-      subtitle: subtitle != null
-          ? Text(subtitle, style: widget.styleDefaultText)
-          : null,
-    );
-  }
-
   List<Widget> _displayDomaineInfo() {
     var listWidget = List<Widget>.of([
-      _constructWidgetInfo(const Icon(Icons.public, size: 40),
-          "${_domaineInfo.nom}.${_domaineInfo.extension}", null,
-          isTitle: true)
+      DomainSpecificInfo(
+        icon: const Icon(Icons.public, size: 40),
+        title: _domaineInfo.nom,
+        subtitle: _domaineInfo.extension,
+        isTitle: true,
+      )
     ]);
     if (!_domaineInfo.isProtected) {
       var dateCreation = DateFormat.yMMMd().format(_domaineInfo.dateCreation!);
       var dateExpiration =
           DateFormat.yMMMd().format(_domaineInfo.dateExpiration!);
       listWidget.addAll([
-        _constructWidgetInfo(const Icon(Icons.wallet, size: 35),
-            "Bénéficiaire: ", _domaineInfo.beneficiaire),
-        _constructWidgetInfo(const Icon(Icons.person, size: 35),
-            "Gestionnaire : ", _domaineInfo.gestionnaire!),
-        _constructWidgetInfo(const Icon(Icons.task_alt, size: 35),
-            "Date de création : ", dateCreation),
-        _constructWidgetInfo(const Icon(Icons.event_busy, size: 35),
-            "Date d'expiration : ", dateExpiration),
-        _constructWidgetInfo(const Icon(Icons.hourglass_bottom, size: 35),
-            "Temps avant expiration", _timeBeforeExpire()),
-        _constructWidgetInfo(
-            const Icon(Icons.dns_rounded, size: 35), "Serveur DNS", _listeDNS())
+        DomainSpecificInfo(
+            icon: const Icon(Icons.wallet, size: 35),
+            title: "Bénéficiaire: ",
+            subtitle: _domaineInfo.beneficiaire),
+        DomainSpecificInfo(
+            icon: const Icon(Icons.person, size: 35),
+            title: "Gestionnaire : ",
+            subtitle: _domaineInfo.gestionnaire!),
+        DomainSpecificInfo(
+            icon: const Icon(Icons.task_alt, size: 35),
+            title: "Date de création : ",
+            subtitle: dateCreation),
+        DomainSpecificInfo(
+            icon: const Icon(Icons.event_busy, size: 35),
+            title: "Date d'expiration : ",
+            subtitle: dateExpiration),
+        DomainSpecificInfo(
+          icon: const Icon(Icons.hourglass_bottom, size: 35),
+          title: "Temps avant expiration",
+          subtitle: _timeBeforeExpire(),
+        ),
+        DomainSpecificInfo(
+          icon: const Icon(Icons.dns_rounded, size: 35),
+          title: "Serveur DNS",
+          subtitle: _listeDNS(),
+        )
       ]);
     } else {
       listWidget.add(
-        _constructWidgetInfo(
-            const Icon(Icons.shield, size: 30), "Ce domaine est protégé", null),
+        const Center(
+          child: Icon(
+            Icons.shield,
+            size: 120,
+          ),
+        ),
+      );
+      listWidget.add(
+        const Center(
+          child: Text(
+            "Ce domaine est protégé",
+          ),
+        ),
       );
     }
     return listWidget;
